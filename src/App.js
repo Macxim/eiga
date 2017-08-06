5f43539a461ca3ddb76fef01864db93bfe600e32
Maxime Laforet <me@macx.im>
2017-08-06 01:19:17 +0100

    Add React Router in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import { PATH_POPULAR, PATH_TOP_RATED, PATH_UPCOMING } from './api';
 
@@ -11,19 +12,21 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <div className="App-main">
-          <div className="App-sidebar-wrapper">
-            <Sidebar />
-          </div>
-          <div className="App-content-wrapper">
-            <Main title="Popular" section={PATH_POPULAR} />
-            <Main title="Top Rated" section={PATH_POPULAR} />
-            <Main title="Coming Soon" section={PATH_UPCOMING} />
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <div className="App-main">
+            <div className="App-sidebar-wrapper">
+              <Sidebar />
+            </div>
+              <div className="App-content-wrapper">
+                <Route exact path="/" render={()=><Main title="Popular" section={PATH_POPULAR} />}/>
+                <Route exact path="/top-rated" render={()=><Main title="Top Rated" section={PATH_TOP_RATED} />}/>
+                <Route exact path="/coming-soon" render={()=><Main title="Coming Soon" section={PATH_UPCOMING} />}/>
+              </div>
           </div>
         </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
