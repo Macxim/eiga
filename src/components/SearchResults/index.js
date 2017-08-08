a610b123bea2047db35b4eb57961c1c4251cc6b1
Maxime Laforet <me@macx.im>
2017-08-08 01:11:23 +0100

    Remove match in SearchResults

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import List from '../../components/List';
 
-const SearchResults = ({ match, location }) => {
+const SearchResults = ({ location }) => {
 
   const result = location.state;
 
