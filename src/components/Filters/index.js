0f31bdf93de1d9d3ac49acd2aa2e47140f115f9b
Maxime Laforet <me@macx.im>
2017-08-14 21:02:10 +0100

    Add Filters component

---
+++ src/components/Filters/index.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import InputRange from 'react-input-range';
+
+import './index.css';
+
+class Filters extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      valueRating: {
+        min: 0,
+        max: 10
+      }
+    };
+
+  }
+
+  render () {
+
+    return (
+      <div className={"App-filters " + (this.props.filtersOpen ? 'is-open' : '')}>
+        <ul className="filters-list">
+          <li className="filters-list__item">
+            <span className="filter-label">
+              Rating from {this.state.valueRating.min} to {this.state.valueRating.max}
+            </span>
+            <InputRange
+              maxValue={10}
+              minValue={0}
+              value={this.state.valueRating}
+              onChange={value => this.setState({ valueRating: value })}
+              onChangeComplete={value => console.log(value)} />
+          </li>
+        </ul>
+      </div>
+    );
+
+  }
+}
+
+export default Filters;
