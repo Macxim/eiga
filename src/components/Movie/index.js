83b7fa0ab15f27722930ca45272c199b3b87dcdc
Maxime Laforet <me@macx.im>
2017-08-25 23:20:53 +0100

    Update styles for Movie component

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -16,7 +16,7 @@ class Movie extends Component {
 
   componentDidMount = () => {
     const MOVIE_ID = this.props.match.params.id;
-    fetch(`${PATH_BASE}${PATH_MOVIE}/${MOVIE_ID}?api_key=${API_KEY}`)
+    fetch(`${PATH_BASE}${PATH_MOVIE}/${MOVIE_ID}?api_key=${API_KEY}&append_to_response=videos`)
     .then(response => response.json())
     .then(movie => (
       this.setState({ movie })
@@ -27,15 +27,23 @@ class Movie extends Component {
 
     const { movie } = this.state;
 
+    console.log(movie);
+    const movieBackdropStyles = {
+      backgroundImage: `url(https://image.tmdb.org/t/p/w1000${movie.backdrop_path})`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover"
+    };
+
     return (
       <div className="Movie-wrapper">
-        <img className="movie-backdrop" src={`https://image.tmdb.org/t/p/w1000${movie.backdrop_path}`} alt=""/>
-        <div className="">
+        <div className="movie-backdrop" style={movieBackdropStyles}></div>
+        <div className="movie-content">
           <img className="movie-poster" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt=""/>
-        </div>
-        <div className="movie-data">
-          <h1 className="movie-title">{movie.title}</h1>
-          <p className="movie-overview">{movie.overview}</p>
+          <div className="movie-data">
+            <h1 className="movie-title">{movie.title}</h1>
+            <h4 className="movie-overview-title">Overview</h4>
+            <p className="movie-overview">{movie.overview}</p>
+          </div>
         </div>
       </div>
     );
