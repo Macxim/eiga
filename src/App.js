0e20271a1b00550cd63d20602a33ad213e066030
Maxime Laforet <me@macx.im>
2017-08-25 23:52:35 +0100

    Add Login component (empty at the moment)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { PATH_POPULAR, PATH_TOP_RATED, PATH_UPCOMING } from './api';
 
 import Header from './components/Header';
+import Login from './components/Login';
 import Footer from './components/Footer';
 import Sidebar from './components/Sidebar';
 import Main from './components/Main';
@@ -23,7 +24,7 @@ class App extends Component {
   }
 
   defaultState = {
-    filtersOpen: false,
+    authenticated: false,
     filters: {
       rating: {
         min: 5,
@@ -53,21 +54,42 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div className="App">
-          <Header />
+          <Header
+            authenticated={this.state.authenticated}
+          />
           <div className="App-main">
             <div className="App-sidebar-wrapper">
-              <Sidebar filters={this.state.filters} updateFilters={this.updateStateWithFilters}                   resetFilters={this.resetFilters} />
+              <Sidebar
+                filters={this.state.filters} updateFilters={this.updateStateWithFilters}                   resetFilters={this.resetFilters} />
               <Footer />
             </div>
               <div className="App-content-wrapper">
-                <Route exact path="/" render={()=><Discover
-                  title="Discover"
-                  updateFilters={this.updateStateWithFilters}
-                  filters={this.state.filters}
-                 />}/>
-                <Route exact path="/popular" render={()=><Main title="Popular" section={PATH_POPULAR} />}/>
-                <Route exact path="/top-rated" render={()=><Main title="Top Rated" section={PATH_TOP_RATED} />}/>
-                <Route exact path="/coming-soon" render={()=><Main title="Coming Soon" section={PATH_UPCOMING} />}/>
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/"
+                  render={()=><Discover
+                    title="Discover"
+                    updateFilters={this.updateStateWithFilters}
+                    filters={this.state.filters}
+                    />}
+                 />
+                <Route exact path="/popular"
+                  render={()=><Main
+                    title="Popular"
+                    section={PATH_POPULAR}
+                    />}
+                />
+                <Route exact path="/top-rated"
+                  render={()=><Main
+                    title="Top Rated"
+                    section={PATH_TOP_RATED}
+                  />}
+                />
+                <Route exact path="/coming-soon"
+                  render={()=><Main
+                    title="Coming Soon"
+                    section={PATH_UPCOMING}
+                  />}
+                />
                 <Route path="/search" component={SearchResults}/>
                 <Route path="/movie/:id-:title" component={Movie}/>
               </div>
---
+++ src/components/Login/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './index.css';
+
+class Login extends Component {
+
+  render () {
+
+    return (
+      <div className="Login-wrapper">
+
+      </div>
+    );
+  }
+}
+
+export default Login;
