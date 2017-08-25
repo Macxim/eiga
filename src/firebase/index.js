ec7ecf6538c6ca97d7d0e8ab0d2299a2f6d9d94c
Maxime Laforet <me@macx.im>
2017-08-25 23:52:51 +0100

    Add Firebase config and package

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyCwagV4zJdRYz_pGXZy-cHEycEXpBMPZ1g",
+  authDomain: "eiga-de72c.firebaseapp.com",
+  databaseURL: "https://eiga-de72c.firebaseio.com",
+  projectId: "eiga-de72c",
+  storageBucket: "eiga-de72c.appspot.com",
+  messagingSenderId: "687294033476"
+};
+
+firebase.initializeApp(config);
