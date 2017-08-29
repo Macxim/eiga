d1efe70e8493db035b2972ecdce8889213ab7d8a
Maxime Laforet <me@macx.im>
2017-08-29 18:43:05 +0100

    Add dropdown UserMenu component

---
+++ src/components/UserMenu/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './index.css';
+
+const UserMenu = (props) => {
+
+  const userMenuClasses = `user-menu ${props.isOpen && 'is-open'}`;
+
+  return (
+    <div className={userMenuClasses}>
+      <span className="user-menu__greetings">{props.username}</span>
+      <ul className="user-menu-list">
+        <li><Link className="user-menu-list__item" to="/favorites">Favorites</Link></li>
+        <li><Link className="user-menu-list__item" to="/watch-list">Watch List</Link></li>
+        <li><Link className="user-menu-list__item user-menu-list__item--logout" to="/logout">Logout</Link></li>
+      </ul>
+    </div>
+  );
+
+}
+
+export default UserMenu;
